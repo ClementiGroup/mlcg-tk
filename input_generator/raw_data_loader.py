4be7c975c1394acddf8e9a41f8b6399e86370789
kbno <bonneau.klara@gmail.com>
2025-03-14 15:49:19 +0100

    added BBA loader

--- input_generator/raw_data_loader.py
+++ input_generator/raw_data_loader.py
@@ -278,6 +278,70 @@ class Cln_loader(DatasetLoader):
         aa_forces = np.concatenate(aa_force_list)
         return aa_coords, aa_forces
 
+class BBA_loader(DatasetLoader):
+    """
+    Loader object for CHARMM22* BBA simulation dataset
+    """
+
+    def get_traj_top(self, name: str, pdb_fn: str):
+        """
+        For a given name, returns a loaded MDTraj object at the input resolution
+        (generally atomistic) as well as the dataframe associated with its topology.
+
+        Parameters
+        ----------
+        name:
+            Name of input sample
+        pdb_fn:
+            Path to pdb structure file
+        """
+        pdb = md.load(pdb_fn.format(name))
+        aa_traj = pdb.atom_slice(
+            [a.index for a in pdb.topology.atoms if a.residue.is_protein]
+        )
+        top_dataframe = aa_traj.topology.to_dataframe()[0]
+        return aa_traj, top_dataframe
+
+    def load_coords_forces(
+        self, base_dir: str, name: str,  stride: int = 1
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        For a given name, returns np.ndarray's of its coordinates and forces at
+        the input resolution (generally atomistic)
+
+        Parameters
+        ----------
+        base_dir:
+            Path to coordinate and force files
+        name:
+            Name of input sample
+        """
+        coords_fns = natsorted(
+            glob(
+                os.path.join(base_dir, f"coords_nowater/bba_coor_folding-bba_*.npy")
+            )
+        )
+
+        forces_fns = [
+            fn.replace(
+                "coords_nowater/bba_coor_folding", "forces_nowater/bba_force_folding"
+            )
+            for fn in coords_fns
+        ]
+
+        aa_coord_list = []
+        aa_force_list = []
+        # load the files, checking against the mol dictionary
+        for cfn, ffn in tqdm(zip(coords_fns, forces_fns), total=len(coords_fns)):
+            force = np.load(ffn)  # in AA
+            coord = np.load(cfn)  # in kcal/mol/AA
+
+            assert coord.shape == force.shape
+            aa_coord_list.append(coord[::stride])
+            aa_force_list.append(force[::stride])
+        aa_coords = np.concatenate(aa_coord_list)
+        aa_forces = np.concatenate(aa_force_list)
+        return aa_coords, aa_forces
 
 class Villin_loader(DatasetLoader):
     def get_traj_top(self, name: str, pdb_fn: str):
