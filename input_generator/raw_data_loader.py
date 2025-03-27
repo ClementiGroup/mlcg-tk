f27efee8c8e073818be04ba3f28e47a13de80672
kbno <bonneau.klara@gmail.com>
2025-03-27 13:48:04 +0100

    added new loaders

--- input_generator/raw_data_loader.py
+++ input_generator/raw_data_loader.py
@@ -153,6 +153,82 @@ class CATH_loader(DatasetLoader):
         aa_forces = np.concatenate(aa_force_list)[::stride]
         return aa_coords, aa_forces
 
+class CATH_ext_loader(DatasetLoader):
+    """
+    Loader object for extended dataset of CATH domain proteins
+    """
+
+    def get_traj_top(self, name: str, pdb_fn: str):
+        """
+        For a given CATH domain name, returns a loaded MDTraj object at the input resolution
+        (generally atomistic) as well as the dataframe associated with its topology.
+
+        Parameters
+        ----------
+        name:
+            Name of input sample
+        pdb_fn:
+            Path to pdb structure file
+        """
+        pdb_fns = glob(pdb_fn.format(name))
+        pdb = md.load(pdb_fns[0])
+        aa_traj = pdb.atom_slice(
+            [a.index for a in pdb.topology.atoms if a.residue.is_protein]
+        )
+        top_dataframe = aa_traj.topology.to_dataframe()[0]
+        return aa_traj, top_dataframe
+
+    def load_coords_forces(
+        self,
+        base_dir: str,
+        name: str,
+        stride: int = 1,
+        batch: Optional[int] = None,
+        n_batches: Optional[int] = 1,
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        For a given CATH domain name, returns np.ndarray's of its coordinates and forces at
+        the input resolution (generally atomistic)
+
+        Parameters
+        ----------
+        base_dir:
+            Path to coordinate and force files
+        name:
+            Name of input sample
+        """
+        if n_batches > 1:
+            raise NotImplementedError(
+                "mol_num_batches can only be used for single-protein datasets for now"
+            )
+        traj_dirs = glob(os.path.join(base_dir, f"group_*/{name}_*/"))
+        all_coords = []
+        all_forces = []
+        for traj_dir in tqdm(traj_dirs):
+            traj_coords = []
+            traj_forces = []
+            fns = glob(os.path.join(traj_dir, "prod_out_full_output/*.npz"))
+            fns.sort(key=lambda file: int(file.split("_")[-2]))
+            last_parent_id = None
+            for fn in fns:
+                np_dict = np.load(fn, allow_pickle=True)
+                current_id = np_dict["id"]
+                parent_id = np_dict["parent_id"]
+                if parent_id is not None:
+                    assert parent_id == last_parent_id
+                traj_coords.append(np_dict["coords"])
+                traj_forces.append(np_dict["Fs"])
+                last_parent_id = current_id
+            traj_full_coords = np.concatenate(traj_coords)
+            traj_full_forces = np.concatenate(traj_forces)
+            if traj_full_coords.shape[0] != 25000:
+                continue
+            else:
+                all_coords.append(traj_full_coords[::stride])
+                all_forces.append(traj_full_forces[::stride])
+        full_coords = np.concatenate(all_coords)
+        full_forces = np.concatenate(all_forces)
+        return full_coords, full_forces
 
 class DIMER_loader(DatasetLoader):
     """
@@ -212,6 +288,78 @@ class DIMER_loader(DatasetLoader):
         with h5py.File(os.path.join(base_dir, "allatom.h5"), "r") as data:
             coord = data["MINI"][name]["aa_coords"][:][::stride]
             force = data["MINI"][name]["aa_forces"][:][::stride]
+
+        # convert to kcal/mol/angstrom and angstrom
+        # from kJ/mol/nm and nm
+        coord = coord * 10
+        force = force / 41.84
+
+        return coord, force
+
+class DIMER_ext_loader(DatasetLoader):
+    """
+    Loader object for extended dataset of mono- and dipeptide pairwise umbrella sampling simulations
+    """
+
+    def get_traj_top(self, name: str, pdb_fn: str):
+        """
+        For a given DIMER pair name, returns a loaded MDTraj object at the input resolution
+        (generally atomistic) as well as the dataframe associated with its topology.
+
+        Parameters
+        ----------
+        name:
+            Name of input sample
+        pdb_fn:
+            Path to pdb structure file
+        """
+        pdb_fns = glob(pdb_fn.format(name))
+        if len(pdb_fns) == 1:
+            pdb = md.load(pdb_fns[0])
+        elif len(pdb_fns) > 1:
+            warnings.warn(
+                f"Pattern {pdb_fn.format(name)} has more than one result",
+                category=UserWarning,
+            )
+        else:
+            return None, None
+        aa_traj = pdb.atom_slice(
+            [a.index for a in pdb.topology.atoms if a.residue.is_protein]
+        )
+        top_dataframe = aa_traj.topology.to_dataframe()[0]
+        return aa_traj, top_dataframe
+
+    def load_coords_forces(
+        self,
+        base_dir: str,
+        name: str,
+        stride: int = 1,
+        batch: Optional[int] = None,
+        n_batches: Optional[int] = 1,
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        For a given DIMER pair name, returns np.ndarray's of its coordinates and forces at
+        the input resolution (generally atomistic)
+
+        Parameters
+        ----------
+        base_dir:
+            Path to coordinate and force files
+        name:
+            Name of input sample
+        """
+        if n_batches > 1:
+            raise NotImplementedError(
+                "mol_num_batches can only be used for single-protein datasets for now"
+            )
+        coord = np.load(
+            glob(os.path.join(base_dir, f"dip_dimers_*/data/{name}_coord.npy"))[0],
+            allow_pickle=True,
+        )[::stride]
+        force = np.load(
+            glob(os.path.join(base_dir, f"dip_dimers_*/data/{name}_force.npy"))[0],
+            allow_pickle=True,
+        )[::stride]
 
         # convert to kcal/mol/angstrom and angstrom
         # from kJ/mol/nm and nm
@@ -466,6 +614,186 @@ class BBA_loader(DatasetLoader):
         aa_forces = np.concatenate(aa_force_list)
         return aa_coords, aa_forces
 
+class ProteinG_loader(DatasetLoader):
+    """
+    Loader object for CHARMM22* Protein G simulation dataset
+    """
+
+    def get_traj_top(self, name: str, pdb_fn: str):
+        """
+        For a given name, returns a loaded MDTraj object at the input resolution
+        (generally atomistic) as well as the dataframe associated with its topology.
+
+        Parameters
+        ----------
+        name:
+            Name of input sample
+        pdb_fn:
+            Path to pdb structure file
+        """
+        pdb = md.load(pdb_fn.format(name))
+        aa_traj = pdb.atom_slice(
+            [a.index for a in pdb.topology.atoms if a.residue.is_protein]
+        )
+        top_dataframe = aa_traj.topology.to_dataframe()[0]
+        return aa_traj, top_dataframe
+
+    def load_coords_forces(
+        self, 
+        base_dir: str, 
+        name: str,  
+        stride: int = 1, 
+        batch: Optional[int] = None, 
+        n_batches: Optional[int] = 1
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        For a given name, returns np.ndarray's of its coordinates and forces at
+        the input resolution (generally atomistic)
+
+        Parameters
+        ----------
+        base_dir:
+            Path to coordinate and force files
+        name:
+            Name of input sample
+        stride : int
+            Interval by which to stride loaded data
+        batch: int or None
+            if trajectories are loaded by batch, indicates the batch index to load
+            must be set if n_batches > 1
+        n_batches: int
+            if greater than 1, divide the total trajectories to load into n_batches chunks
+        """
+        coords_fns = natsorted(
+            glob(
+                os.path.join(base_dir, f"coords_3_nowater/proteing_coor_folding-proteing-*.npy")
+            )
+        )
+
+        forces_fns = [
+            fn.replace(
+                "coords_3_nowater/proteing_coor_folding", "forces_3_nowater/proteing_force_folding"
+            )
+            for fn in coords_fns
+        ]
+
+        coords_fns = np.array(coords_fns)
+        forces_fns = np.array(forces_fns)
+
+        if n_batches > 1:
+            assert batch is not None, "batch id must be set if more than 1 batch"
+            chunk_ids = chunker([i for i in range(len(coords_fns))], n_batches=n_batches)
+            coords_fns = coords_fns[np.array(chunk_ids[batch])]
+            forces_fns = forces_fns[np.array(chunk_ids[batch])]
+
+        aa_coord_list = []
+        aa_force_list = []
+        # load the files, checking against the mol dictionary
+        for cfn, ffn in tqdm(zip(coords_fns, forces_fns), total=len(coords_fns)):
+            force = np.load(ffn)  # in AA
+            coord = np.load(cfn)  # in kcal/mol/AA
+
+            assert coord.shape == force.shape
+            aa_coord_list.append(coord[::stride])
+            aa_force_list.append(force[::stride])
+        aa_coords = np.concatenate(aa_coord_list)
+        aa_forces = np.concatenate(aa_force_list)
+        return aa_coords, aa_forces
+
+class A3D_loader(DatasetLoader):
+    """
+    Loader object for CHARMM22* A3D simulation dataset
+    """
+
+    def get_traj_top(self, name: str, pdb_fn: str):
+        """
+        For a given name, returns a loaded MDTraj object at the input resolution
+        (generally atomistic) as well as the dataframe associated with its topology.
+
+        Parameters
+        ----------
+        name:
+            Name of input sample
+        pdb_fn:
+            Path to pdb structure file
+        """
+        pdb = md.load(pdb_fn.format(name))
+        aa_traj = pdb.atom_slice(
+            [a.index for a in pdb.topology.atoms if a.residue.is_protein]
+        )
+        top_dataframe = aa_traj.topology.to_dataframe()[0]
+        return aa_traj, top_dataframe
+
+    def load_coords_forces(
+        self, 
+        base_dir: str, 
+        name: str,  
+        stride: int = 1, 
+        batch: Optional[int] = None, 
+        n_batches: Optional[int] = 1
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        For a given name, returns np.ndarray's of its coordinates and forces at
+        the input resolution (generally atomistic)
+
+        Parameters
+        ----------
+        base_dir:
+            Path to coordinate and force files
+        name:
+            Name of input sample
+        stride : int
+            Interval by which to stride loaded data
+        batch: int or None
+            if trajectories are loaded by batch, indicates the batch index to load
+            must be set if n_batches > 1
+        n_batches: int
+            if greater than 1, divide the total trajectories to load into n_batches chunks
+        """
+        coords_fns = natsorted(
+            glob(
+                os.path.join(base_dir, f"coords_*_nowater/a3D_coor_folding-a3d-*.npy")
+            )
+        )
+        forces_fns = []
+        for coord_fn in coords_fns:
+            if "coords_1_nowater" in coord_fn:
+                forces_fns.append(
+                    coord_fn.replace(
+                        "coords_1_nowater/a3D_coor_folding", "forces_1_nowater/a3D_force_folding"
+                    )
+                )
+            elif "coords_2_nowater" in coord_fn:
+                forces_fns.append(
+                    coord_fn.replace(
+                        "coords_2_nowater/a3D_coor_folding", "forces_2_nowater/a3D_force_folding"
+                    )
+                )
+            else:
+                raise RuntimeError("wrong path in A3D loader")
+        
+        coords_fns = np.array(coords_fns)
+        forces_fns = np.array(forces_fns)
+
+        if n_batches > 1:
+            assert batch is not None, "batch id must be set if more than 1 batch"
+            chunk_ids = chunker([i for i in range(len(coords_fns))], n_batches=n_batches)
+            coords_fns = coords_fns[np.array(chunk_ids[batch])]
+            forces_fns = forces_fns[np.array(chunk_ids[batch])]
+
+        aa_coord_list = []
+        aa_force_list = []
+        # load the files, checking against the mol dictionary
+        for cfn, ffn in tqdm(zip(coords_fns, forces_fns), total=len(coords_fns)):
+            force = np.load(ffn)  # in AA
+            coord = np.load(cfn)  # in kcal/mol/AA
+
+            assert coord.shape == force.shape
+            aa_coord_list.append(coord[::stride])
+            aa_force_list.append(force[::stride])
+        aa_coords = np.concatenate(aa_coord_list)
+        aa_forces = np.concatenate(aa_force_list)
+        return aa_coords, aa_forces
 
 class Villin_loader(DatasetLoader):
     def get_traj_top(self, name: str, pdb_fn: str):
